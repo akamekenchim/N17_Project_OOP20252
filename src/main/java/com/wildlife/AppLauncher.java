27670f51a6cebb27b2547aa812fa29760c38723a
KenChim <giaanhnp@gmail.com>
2026-03-23 16:30:48 +0700

    Added a blank canvas - linking the engine and world map together

--- src/main/java/com/wildlife/AppLauncher.java
+++ src/main/java/com/wildlife/AppLauncher.java
@@ -2,7 +2,8 @@ package com.wildlife;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        // tạm thời gọi main của testgraphics
-        TestGraphics.main(args); 
+        // tạm thời gọi main của apprunner
+        // nếu ae muốn kiểm tra việc chạy sprite, đổi apprunner thành TestGraphics. Sau đó đổi lại về AppRunner, mình sẽ build trên đó
+        AppRunner.main(args); 
     }
 }
---
+++ src/main/java/com/wildlife/AppRunner.java
@@ -0,0 +1,36 @@
+package com.wildlife;
+
+import com.wildlife.core.Constants;
+import com.wildlife.core.SimEngine;
+import com.wildlife.core.WorldMap;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AppRunner extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        Canvas cv = new Canvas(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT); //tạo canvas hoàn toàn trống
+        GraphicsContext gc = cv.getGraphicsContext2D(); //Lấy graphicscontext của canvas vừa tạo
+
+        WorldMap map = new WorldMap();
+        SimEngine engine = new SimEngine(map, gc);
+
+        engine.Start(); //Trong hàm start này có 3 việc: Xóa màn, update trạng thái của WorldMap, rồi render các thực thể
+
+        Group gr = new Group(cv);
+        Scene scene = new Scene(gr, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
