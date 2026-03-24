e693332c3efa24f98dbf6ca7ecefc28e12fab59e
KenChim <giaanhnp@gmail.com>
2026-03-24 14:04:11 +0700

    Changed the constants to final

--- src/main/java/com/wildlife/core/Constants.java
+++ src/main/java/com/wildlife/core/Constants.java
@@ -1,8 +1,8 @@
 package com.wildlife.core;
 //file này là chứa các hằng số, ae có thể thêm tốc độ của mấy con vật vào đây 
 public class Constants {
-    public static double SCREEN_WIDTH = 1100;
-    public static double SCREEN_HEIGHT = 800;
-    public static double RABBIT_SPEED = 2.0;
-    public static double WOLF_SPEED = 3.5;
+    public static final double SCREEN_WIDTH = 1100;
+    public static final double SCREEN_HEIGHT = 800;
+    public static final double RABBIT_SPEED = 2.0;
+    public static final double WOLF_SPEED = 3.5;
 }
